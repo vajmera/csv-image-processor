3cd23434e16b10f5f2d1363a2d2385ea2c6ba89d
Ajmera <f20170693@pilani.bits-pilani.ac.in>
2024-09-11 18:50:55 +0530

    first commit

---
+++ src/main/java/com/example/csvimage/CsvimageApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvimageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csvimage/CsvimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
